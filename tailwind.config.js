a4bb533980ae9de477adc2477ff5fe6e07adffc4
Keir Forster <keirforster@gmail.com>
2022-04-21 19:39:30 -0700

    build: configure inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
+  important: true,
   content: ["./src/**/*.{html,ts,css}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: `"Inter var", ${defaultTheme.fontFamily.sans.join(",")}`,
+      },
+    },
   },
   plugins: [],
 };
