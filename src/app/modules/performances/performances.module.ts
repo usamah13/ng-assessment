d3cef9b03ecb1dc0f9dcc19e64e3743281b1d310
Keir Forster <keirforster@gmail.com>
2022-04-27 15:59:56 -0700

    refactor: change import path from absolute to relative

--- src/app/modules/performances/performances.module.ts
+++ src/app/modules/performances/performances.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { SharedModule } from 'src/app/shared/shared.module';
 
+import { SharedModule } from '../../shared/shared.module';
 import { PerformancesComponent } from './performances.component';
 import { performancesRoutes } from './performances.routing';
 
