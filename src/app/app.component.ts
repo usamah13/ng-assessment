d7d4a3d6f8c9a721825056852aab372d7b64264f
Keir Forster <keirforster@gmail.com>
2022-04-27 09:32:12 -0700

    refactor: add menu state and content layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: "app-root",
   templateUrl: "app.component.html",
   styles: [],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "ng-assessment";
+
+  menuIsOpen: boolean;
+
+  constructor() {
+    this.menuIsOpen = false;
+  }
+
+  ngOnInit(): void {}
+
+  onToggleMenu(): void {
+    this.menuIsOpen = !this.menuIsOpen;
+  }
 }
