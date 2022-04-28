05c326ef24cc1ceffa8d6ac5b3b4bd65aa7489e7
Keir Forster <keirforster@gmail.com>
2022-04-27 23:14:34 -0700

    perf(performances): add trackby and onpush configurations

--- src/app/modules/performances/performances.component.ts
+++ src/app/modules/performances/performances.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { KeyValue } from "@angular/common";
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from "@angular/core";
 import { Observable, Subscription } from "rxjs";
 
 import { Performance } from "./performance.type";
@@ -7,7 +8,6 @@ import { PerformancesService } from "./performances.service";
 export type SortDirection = "time:slowToFast" | "time:fastToSlow" | "year:frToSr" | "year:srToFr";
 
 interface MenuOption {
-  label: string;
   active: boolean;
   sortBy: SortDirection;
   showDivider?: boolean;
@@ -17,62 +17,59 @@ interface MenuOption {
   selector: "app-performances",
   templateUrl: "performances.component.html",
   styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PerformancesComponent implements OnInit, OnDestroy {
-  private subscription: Subscription;
+  readonly performances$: Observable<Performance[]>;
+  readonly menuOptions: Map<string, MenuOption>;
 
-  performances$: Observable<Performance[]>;
-  menuOptions: { [label: string]: MenuOption };
+  private readonly subscription: Subscription;
 
   constructor(private performancesService: PerformancesService) {
     this.subscription = new Subscription();
     this.performances$ = this.performancesService.performances$;
-    this.menuOptions = {
-      "Time: Fast to Slow": {
-        active: false,
-        label: "Time: Fast to Slow",
-        sortBy: "time:fastToSlow",
-      },
-      "Time: Slow to Fast": {
-        active: false,
-        label: "Time: Slow to Fast",
-        sortBy: "time:slowToFast",
-      },
-      "Year: Freshman to Senior": {
-        active: true,
-        label: "Year: Freshman to Senior",
-        sortBy: "year:frToSr",
-        showDivider: true,
-      },
-      "Year: Senior to Freshman": {
-        active: false,
-        label: "Year: Senior to Freshman",
-        sortBy: "year:srToFr",
-      },
-    };
+    this.menuOptions = new Map();
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initializeMenu();
+  }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
 
-  onMenuItemClick(selectedOption: MenuOption): void {
+  onMenuItemClick(selectedSortLabel: string, selectedOption: MenuOption): void {
     // set all options to false
-    for (const o of Object.values(this.menuOptions)) this.menuOptions[o.label].active = false;
+    for (const [label, menuOption] of this.menuOptions.entries())
+      this.menuOptions.get(label).active = false;
 
-    if (!this.menuOptions[selectedOption.label].active) {
+    if (!this.menuOptions.get(selectedSortLabel).active) {
       this.getSortedPerformances(selectedOption.sortBy);
     }
 
     // set the selected option to active
-    this.menuOptions[selectedOption.label].active = true;
+    this.menuOptions.get(selectedSortLabel).active = true;
+  }
+
+  trackByMenuOption(index: number, menuOption: KeyValue<string, MenuOption>): string {
+    return menuOption.key;
+  }
+
+  trackByPerformance(index: number, performance: Performance): string {
+    return performance.id;
   }
 
   private getSortedPerformances(sortDirection: SortDirection): void {
     this.subscription.add(
       this.performancesService.getSortedPerformances(sortDirection).subscribe()
     );
+  }
+
+  private initializeMenu(): void {
+    this.menuOptions.set("Time: Fast to Slow", { active: false, sortBy: "time:fastToSlow" });
+    this.menuOptions.set("Time: Slow to Fast", { active: false, sortBy: "time:slowToFast" });
+    this.menuOptions.set("Year: Freshman to Senior", { active: true, sortBy: "year:frToSr" });
+    this.menuOptions.set("Year: Senior to Freshman", { active: false, sortBy: "year:srToFr" });
   }
 }
